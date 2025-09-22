ca1b09b130452ae7edd5c76e7556fecd1d4b43ea
Qtynyambu <nyamburamercy229gmail.com>
2025-09-22 11:59:51 +0300

    Message

---
+++ conf.php
@@ -0,0 +1,15 @@
+<?php
+// conf.php
+
+// Database configuration
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '1234');
+define('DB_NAME', 'ics_project');
+
+// Email configuration
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_USERNAME', 'mercy.ngure@strathmore.edu');
+define('SMTP_PASSWORD', 'kgdi ovgr ralk dygt'); // Use your generated App Password
+define('SMTP_PORT', 465);
+?>
